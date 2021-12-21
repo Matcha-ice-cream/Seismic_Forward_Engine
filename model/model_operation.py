a86e4cf6dfb5ee9d591e348b4c37a8afc791cd8d
miaoziemm <changzm2316@126.com>
2021-12-21 21:22:26 +0800

    update

--- model/model_operation.py
+++ model/model_operation.py
@@ -19,7 +19,13 @@ class getmodel:
         self.model_vz1 = ti.field(dtype=ti.f32, shape=(nx, nz))
         self.model_vz2 = ti.field(dtype=ti.f32, shape=(nx, nz))
 
-        self.model_rand = ti.Vector.field(2, dtype=ti.f32, shape=(50, 50))
+        self.model_munk = ti.field(dtype=ti.f32, shape=(nx, nz))
+
+        # self.model_rand = ti.Vector.field(2, dtype=ti.f32, shape=(50, 50))
+        self.model_rand = ti.Vector.field(2, ti.f32)
+        ti.root.dense(ti.i, 100).dense(ti.j, 100).place(self.model_rand)
+
+
 
     def diff_1(self, v):
         for i, j in v:
@@ -80,6 +86,7 @@ class getmodel:
             # assert "Please make sure that the small grid is divisible./请确保小格子可被整除.(来自喵子emm的善意提醒)"
             pass
         # ti.root.dense(ti.ij, (self.nx / lx + 1, self.nz / lz + 1)).place(self.model_rand)
+        # ti.root.dense(ti.i, self.nx / lx + 1).dense(ti.j, self.nz / lz + 1).place(self.model_rand)
         for i, j in self.model_rand:
             a = (ti.random(ti.f32)-0.5)*2
             b = (ti.random(ti.f32)-0.5)*2
@@ -111,6 +118,43 @@ class getmodel:
             self.model_vs[i, j] = u
             self.model_rho[i, j] = u
 
+    @ti.kernel
+    def model_perlin_munk(self, lx: ti.i32, lz: ti.i32):
+        if self.nx / lx != 0 or self.nz / lz != 0:
+            # assert "Please make sure that the small grid is divisible./请确保小格子可被整除.(来自喵子emm的善意提醒)"
+            pass
+        # ti.root.dense(ti.ij, (self.nx / lx + 1, self.nz / lz + 1)).place(self.model_rand)
+        # ti.root.dense(ti.i, self.nx / lx + 1).dense(ti.j, self.nz / lz + 1).place(self.model_rand)
+        for i, j in self.model_rand:
+            a = (ti.random(ti.f32) - 0.5) * 2
+            b = (ti.random(ti.f32) - 0.5) * 2
+            self.model_rand[i, j] = ti.Vector([a, b]).normalized()
+
+        for i, j in self.model_vp:
+            xn = int(ti.floor(i / lx))
+            zn = int(ti.floor(j / lz))
+            xi = i % lx
+            zi = j % lz
+            xf = float(xi) / float(lx)
+            zf = float(zi) / float(lz)
+            xt = self.fade(xf)
+            zt = self.fade(zf)
+            Pa = ti.Vector([xf, zf])
+            Pb = ti.Vector([xf - 1.0, zf])
+            Pc = ti.Vector([xf, zf - 1.0])
+            Pd = ti.Vector([xf - 1.0, zf - 1.0])
+            TA = self.model_rand[xn, zn].dot(Pa)
+            TB = self.model_rand[xn + 1, zn].dot(Pb)
+            TC = self.model_rand[xn, zn + 1].dot(Pc)
+            TD = self.model_rand[xn + 1, zn + 1].dot(Pd)
+
+            l1 = TA + (TB - TA) * xt
+            l2 = TC + (TD - TC) * xt
+            u = l1 + (l2 - l1) * zt
+
+            self.model_vp[i, j] = u
+            self.model_vs[i, j] = u
+            self.model_rho[i, j] = u
 
     @ti.kernel
     def model_perlin_change(self):
--- model_build_example.py
+++ model_build_example.py
@@ -6,12 +6,11 @@ ti.init(arch=ti.gpu)
 
 model_cs = getmodel(500, 500, 10, 10)
 
-
 model_cs.model_perlin(50, 50)
 
 gui = ti.GUI("model", (500, 500))
 
-tools.export(model_cs.model_vp, "data.txt")
+# tools.export(model_cs.model_vp, "data.txt")
 while True:
     gui.set_image(model_cs.model_vp)
     gui.show()
