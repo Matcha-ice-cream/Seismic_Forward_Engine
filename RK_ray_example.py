b15f59f9402fd43cb084f8041caad3bfd44b828b
miaoziemm <changzm2316@126.com>
2021-12-30 11:04:45 +0800

    update

---
+++ RK_ray_example.py
@@ -0,0 +1,21 @@
+import taichi as ti
+from model.model_operation import getmodel
+
+
+ti.init(arch=ti.gpu)
+
+## 参数初始化
+nx = 500
+nz = 500
+dx = 10
+dz = 10
+
+
+## 模型建立
+model_cs = getmodel(nx, nz, dx, dz)
+model_cs.model_perlin_munk(dx*5, dx*5, 1000.0, 1000.0, 1500.0)
+model_v = model_cs.model_vp
+
+
+
+
--- ray/RK_ray.py
+++ ray/RK_ray.py
@@ -1,4 +1,5 @@
 import taichi as ti
+import Tools.SFE_math as Smath
 
 @ti.data_oriented
 class RK_ray:
@@ -11,7 +12,23 @@ class RK_ray:
         self.dx = dx
         self.ray_n = ray_n
 
-        
+    @ti.func
+    def RK(self, X, v, v_dx, v_dz):
+        Xp = ti.Vector([0.0, 0.0, 0.0, 0.0])
+        x0 = X[0]
+        z0 = X[1]
+        px0 = X[2]
+        pz0 = X[3]
+
+        ll1 = px0
+        ll2 = pz0
+
+
+
+
+
+
+
 
 
 
